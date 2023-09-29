4b12c44d93d9e0f6de73508acae66cd61afb0679
Ikeji Benjamin <5072922+benmalcom@users.noreply.github.com>
2023-09-29 22:34:12 +0100

    Refactor

--- src/components/layouts/StackedLayout.vue
+++ src/components/layouts/StackedLayout.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div
-    class="flex flex-col w-full xl:max-w-6xl min-h-screen mx-auto px-4 box-border mt-10 sm:mt-20"
+    class="flex flex-col w-full xl:max-w-6xl min-h-screen mx-auto px-4 box-border mt-10 sm:mt-12"
   >
     <slot></slot>
   </div>
