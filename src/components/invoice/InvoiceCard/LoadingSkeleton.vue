00ceb5b2aa360912a3c3e86b141694808a3617cb
Ikeji Benjamin <5072922+benmalcom@users.noreply.github.com>
2023-09-29 23:57:58 +0100

    Stop vite from building old cache

--- src/components/invoice/InvoiceCard/LoadingSkeleton.vue
+++ src/components/invoice/InvoiceCard/LoadingSkeleton.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="loading-card">
     <!-- Loading Skeleton for User Info -->
-    <div class="user-info-skeleton">
-      <div class="user-avatar-skeleton"></div>
-      <div class="user-info-details">
-        <div class="user-name-skeleton">Loading User...</div>
-        <div class="user-status-skeleton"></div>
+    <div class="client-info-skeleton">
+      <div class="client-avatar-skeleton"></div>
+      <div class="client-info-details">
+        <div class="client-name-skeleton">Loading User...</div>
+        <div class="client-status-skeleton"></div>
       </div>
     </div>
 
     <!-- Loading Skeleton for Invoice Details -->
-    <div class="invoice-details-skeleton">
+    <div class="invoice-summary-skeleton">
       <div class="invoice-row-skeleton">
         <div class="invoice-label-skeleton"></div>
         <div class="invoice-value-skeleton"></div>
@@ -29,27 +29,27 @@
   @apply flex flex-col drop-shadow-md;
 }
 
-.user-info-skeleton {
+.client-info-skeleton {
   @apply w-full flex p-5 box-border gap-x-3 items-center bg-gray-200 rounded-t-2xl animate-pulse;
 }
 
-.user-avatar-skeleton {
+.client-avatar-skeleton {
   @apply h-12 w-12 rounded-lg bg-gray-300;
 }
 
-.user-info-details {
+.client-info-details {
   @apply flex-1 flex justify-between gap-x-2 h-full items-center;
 }
 
-.user-name-skeleton {
+.client-name-skeleton {
   @apply font-semibold text-white flex-1;
 }
 
-.user-status-skeleton {
+.client-status-skeleton {
   @apply w-8 h-2 bg-gray-300;
 }
 
-.invoice-details-skeleton {
+.invoice-summary-skeleton {
   @apply w-full p-6 box-border bg-white rounded-b-2xl flex flex-col justify-between;
 }
 
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,24 @@
-import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vite'
-import { fileURLToPath, URL } from 'node:url'
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from 'node:url';
 
+const hash = Math.floor(Math.random() * 90000) + 10000;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name]` + hash + `.js`,
+        chunkFileNames: `[name]` + hash + `.js`,
+        assetFileNames: `[name]` + hash + `.[ext]`
+      }
+    }
   }
-})
+});
